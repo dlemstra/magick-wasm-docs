d40e4fa0573a3aae73548718947fd15d2531420c
Dirk Lemstra <dirk@lemstra.org>
2021-08-19 21:39:58 +0200

    Added drag and drop area to the Canvas.

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas ref="canvas"></canvas>
+  <canvas ref="canvas" @drop.prevent="onDrop" @dragover.prevent></canvas>
 </template>
 
 <script lang="ts">
@@ -16,6 +16,37 @@ export default class Canvas extends Vue {
   }
   write(image: IMagickImage): void {
     image.writeToCanvas(this.getCanvas())
+  }
+  mounted(): void {
+    const canvas = this.getCanvas()
+    canvas.width = 400
+    canvas.height = 400
+
+    const ctx = canvas.getContext("2d")
+    if (ctx === null)
+      return
+
+    ctx.fillStyle = 'rgb(219, 97, 162)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  }
+  private async onDrop(event: DragEvent) {
+    const files = event.dataTransfer?.files
+    if (files === null)
+      return;
+
+    const file = files?.item(0)
+    if (file === null || file === undefined)
+      return
+
+    const arrayBuffer = await file.arrayBuffer()
+    const array = new Uint8Array(arrayBuffer)
+    try {
+      ImageMagick.read(array , (image) => {
+        this.write(image)
+      })
+    } catch (exception) {
+      // unable to read file
+    }
   }
   private getCanvas() {
     return this.$refs.canvas as HTMLCanvasElement
