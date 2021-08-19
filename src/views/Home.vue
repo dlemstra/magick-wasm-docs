30496f860572b630fd7c736102eceb3b3223d48b
Dirk Lemstra <dirk@lemstra.org>
2021-08-19 09:08:33 +0200

    Changed the home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <CodeSample>alert('foo')</CodeSample>
+    <img alt="ImageMagick logo" src="../assets/imagemagick.svg">
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import CodeSample from '@/components/CodeSample.vue'
+import { Vue } from 'vue-class-component';
 
-@Options({
-  components: {
-    HelloWorld,
-    CodeSample
-  },
-})
 export default class Home extends Vue {}
 </script>
