6ad3ffa1624825b17af516421037f1454fde9ec6
Dirk Lemstra <dirk@lemstra.org>
2021-08-19 09:09:11 +0200

    Remove about view.

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,7 @@ import { RouteRecordRaw } from 'vue-router'
 import Home from './views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  { path: '/', component: Home  },
-  { path: '/about', component: () => import(/* webpackChunkName: "about" */ './views/About.vue') }
+  { path: '/', component: Home  }
 ]
 
 export default routes
