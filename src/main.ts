990ec4f2d4f709d890b49cffda4beef6abd4b5e2
Dirk Lemstra <dirk@lemstra.org>
2021-08-19 09:03:22 +0200

    Initalize magick-wasm at the start of the application.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { initializeImageMagick } from '@imagemagick/magick-wasm'
 import App from './App.vue'
 import routes from './routes'
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+initializeImageMagick().then(() => {
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+  const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
+
+  createApp(App)
+    .use(router)
+    .mount('#app')
+
+}).catch(err => {
+  console.error(err)
+})
