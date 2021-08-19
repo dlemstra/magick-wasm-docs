c88692b62e552ea83d4ec0c91930dd4ad721f9d0
Dirk Lemstra <dirk@lemstra.org>
2021-08-19 08:54:05 +0200

    Added component to display a code sample.

---
+++ src/components/CodeSample.vue
@@ -0,0 +1,25 @@
+<template>
+  <ssh-pre language="js" :dark=true>
+    {{code}}
+  </ssh-pre>
+</template>
+
+<script lang="ts">
+import 'simple-syntax-highlighter/dist/sshpre.css'
+import { Options, Vue } from 'vue-class-component'
+import SshPre from 'simple-syntax-highlighter'
+
+@Options({
+  computed: {
+    code() { 
+      if (this.$slots.default === undefined)
+        return ''
+      return this.$slots.default()[0].children
+    }
+  },
+  components: {
+    SshPre
+  }
+})
+export default class CodeSample extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <CodeSample>alert('foo')</CodeSample>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import CodeSample from '@/components/CodeSample.vue'
 
 @Options({
   components: {
     HelloWorld,
+    CodeSample
   },
 })
 export default class Home extends Vue {}
