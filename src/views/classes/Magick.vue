20348614c5ce983edd2d0cd7d397fee45509fef1
Dirk Lemstra <dirk@lemstra.org>
2021-08-20 10:27:33 +0200

    Moved methods to class.

--- src/views/classes/Magick.vue
+++ src/views/classes/Magick.vue
@@ -1,16 +1,16 @@
 <template>
   <h1>Magick</h1>
   <h3>.delegates</h3>
-  <CodeSample :code=delegates />
+  <CodeSample :code=delegates() />
 
   <h3>.features</h3>
-  <CodeSample :code=features />
+  <CodeSample :code=features() />
 
   <h3>.imageMagickVersion</h3>
-  <CodeSample :code=imagemagickVersion />
+  <CodeSample :code=imagemagickVersion() />
 
   <h3>.supportedFormats</h3>
-  <CodeSample :code=supportedFormats />
+  <CodeSample :code=supportedFormats() />
 </template>
 
 <script lang="ts">
@@ -18,24 +18,25 @@ import { Options, Vue } from 'vue-class-component'
 import { Magick } from '@imagemagick/magick-wasm/magick'
 import CodeSample from '@/components/CodeSample.vue'
 
-function getFormatsArray() {
-  return Magick.supportedFormats
-    .map(function(format) {
-      const description = format.description.replace('\'', '\\\'')
-      return `  { format: '${format.format}', description: '${description}', isReadable: ${format.isReadable}, isWritable: ${format.isWritable} }`
-    }).join(",\n")
-}
-
 @Options({
   components: {
     CodeSample,
   },
   computed: {
-    delegates() { return `> '${Magick.delegates}'` },
-    features() { return `> '${Magick.features}'` },
-    imagemagickVersion() { return `> '${Magick.imageMagickVersion}'` },
-    supportedFormats() { return `[\n${getFormatsArray()}\n]` }
   }
 })
-export default class MagickView extends Vue {}
+export default class MagickView extends Vue {
+    delegates(): string { return `> '${Magick.delegates}'` }
+    features(): string { return `> '${Magick.features}'` }
+    imagemagickVersion(): string { return `> '${Magick.imageMagickVersion}'` }
+    supportedFormats(): string { return `[\n${this.getFormatsArray()}\n]` }
+
+    private getFormatsArray() {
+      return Magick.supportedFormats
+        .map(function(format) {
+          const description = format.description.replace('\'', '\\\'')
+          return `  { format: '${format.format}', description: '${description}', isReadable: ${format.isReadable}, isWritable: ${format.isWritable} }`
+        }).join(",\n")
+    }
+}
 </script>
