eeefa1642dcfc65f83f5bdc76b84de05fad66984
Dirk Lemstra <dirk@lemstra.org>
2021-08-20 14:17:03 +0200

    Whitespace.

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    publicPath: '/magick-wasm-docs/',
-    chainWebpack: config => {
-        const size = 100000000;
-        config.performance
-            .maxEntrypointSize(size)
-            .maxAssetSize(size)
-    }
+  publicPath: '/magick-wasm-docs/',
+  chainWebpack: config => {
+    const size = 100000000;
+    config.performance
+      .maxEntrypointSize(size)
+      .maxAssetSize(size)
+  }
 }
