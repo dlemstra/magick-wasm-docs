10bce7b97849688354e7b4f2ba95fedd3d4f6805
Dirk Lemstra <dirk@lemstra.org>
2022-04-28 10:43:50 +0200

    Use axios to download the font and add it to the Magick fonts.

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { initializeImageMagick } from '@imagemagick/magick-wasm'
 import { Magick } from '@imagemagick/magick-wasm/magick'
 import App from './App.vue'
+import axios from 'axios'
 import routes from './routes'
-import font from '!!url-loader!./assets/fonts/Hack-Regular.ttf'
 
 initializeImageMagick().then(() => {
-    const fontString = atob(font.substring(21))
-    const data = new Uint8Array(fontString.length)
-    for (let i = 0; i < data.length; i++)
-        data[i] = fontString.charCodeAt(i)
+    axios({
+        url: new URL('./assets/fonts/Hack-Regular.ttf', import.meta.url).href,
+        method: 'GET',
+        responseType: 'blob',
+    }).then(async (response) => {
+        const data = await response.data.arrayBuffer()
+        Magick.addFont('Hack', new Uint8Array(data))
 
-    Magick.addFont('Hack', data)
+        const router = createRouter({
+            history: createWebHistory(process.env.BASE_URL),
+            routes
+        })
 
-    const router = createRouter({
-        history: createWebHistory(process.env.BASE_URL),
-        routes
-    })
-
-    createApp(App)
-        .use(router)
-        .mount('#app')
+        createApp(App)
+            .use(router)
+            .mount('#app')
+    });
 }).catch(err => {
     throw err
 })
