f051381baa27a6aa6df0d40f901e453fc1c44ae0
Dirk Lemstra <dirk@lemstra.org>
2022-04-28 09:40:24 +0200

    Configured polyfills.

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,17 @@ module.exports = {
         config.performance
             .maxEntrypointSize(size)
             .maxAssetSize(size);
+    },
+    configureWebpack: {
+        resolve: {
+            fallback: {
+                "buffer": false,
+                "child_process": false,
+                "crypto": require.resolve("crypto-browserify"),
+                "fs": false,
+                "path": require.resolve("path-browserify"),
+                "stream": require.resolve("stream-browserify")
+            }
+        }
     }
 }
