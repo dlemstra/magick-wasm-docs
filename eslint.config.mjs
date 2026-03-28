661a599cc5f2885407f98b37ce2b46fb4dbb81e7
Dirk Lemstra <dirk@lemstra.org>
2026-03-28 11:02:35 +0100

    Updated eslint and it's config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,26 @@
-import js from "@eslint/js";
-import path from "node:path";
+import { defineConfig } from "eslint/config";
 import pluginLicenseHeader from "eslint-plugin-license-header";
 import pluginTypescriptEslint from "@typescript-eslint/eslint-plugin";
-import pluginVue from "eslint-plugin-vue";
 import tsParser from "@typescript-eslint/parser";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: [
-        "**/dist",
-        "*.mjs",
+export default defineConfig([{
+    files: [
+        "**/*.ts"
     ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-), {
+
+    ignores: [
+        "dist/*",
+    ],
+
     plugins: {
         "@eslint-plugin-license-header": pluginLicenseHeader,
-        "@eslint-plugin-vue": pluginVue,
         "@typescript-eslint": pluginTypescriptEslint,
     },
+
+    extends: [
+        "@typescript-eslint/eslint-recommended",
+        "@typescript-eslint/recommended",
+    ],
 
     languageOptions: {
         parser: tsParser,
@@ -56,4 +47,4 @@ export default [{
         ],
         "@typescript-eslint/no-duplicate-enum-values": "off",
     },
-}];
+}]);
